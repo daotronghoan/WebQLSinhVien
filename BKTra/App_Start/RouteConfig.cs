620f149a13bc65db577a686e08fa64a25935cfc1
daotronghoan <daotronghoandz@gmail.com>
2024-08-09 15:17:11 +0700

    xóa các ảnh

--- BKTra/App_Start/RouteConfig.cs
+++ BKTra/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace BKTra
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            //hello
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
